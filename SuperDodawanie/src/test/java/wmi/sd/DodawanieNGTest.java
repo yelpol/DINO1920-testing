1f5929dbc5d175f88e1fffc14e8837698763b394
mikkrz1 <57178153+mikkrz1@users.noreply.github.com>
2019-10-30 23:19:33 +0100

    Testy 3 i 4 dla metody dodawanie z klasy Dodawanie
    Testy 3 i 4 dla metody dodawanie z klasy Dodawanie

--- SuperDodawanie/src/test/java/wmi/sd/DodawanieNGTest.java
+++ SuperDodawanie/src/test/java/wmi/sd/DodawanieNGTest.java
@@ -47,5 +47,30 @@ public class DodawanieNGTest {
             assertEquals(result, expResult);
         }
     }
+    
+    @Test
+    public void testDodawanie3() {
+        System.out.println("dodawanie3");
+        String a = "4.3";
+        String b = "2";
+        String expResult = "6.3";
+        String result = Dodawanie.dodawanie(a, b);
+        assertEquals(result, expResult);
+    }
+    
+    @Test
+    public void testDodawanie4() {
+        System.out.println("dodawanie4");
+        Random  r = new Random();
+        for(int i = 0; i < 5; i++){
+            float aa = r.nextFloat()*100;
+            int bb = r.nextInt(100);
+            String a = Float.toString(aa);
+            String b = Integer.toString(bb);
 
+            String expResult = Float.toString(aa + bb);
+            String result = Dodawanie.dodawanie(a, b);
+            assertEquals(result, expResult);
+        }
+    }
 }
